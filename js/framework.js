3abad07d483a8f95cb739d9c9f6c094ea0fe3e50
James Tomasino <james@tomasino.org>
2017-07-28 23:39:16 -0400

    moving to arrow functions. added isID

--- js/framework.js
+++ js/framework.js
@@ -19,7 +19,7 @@ window.ns = window.ns || function (ns) {
 }
 
 window.ns('org.tomasino.clm').modify({
-  VERSION: '0.1.1',
+  VERSION: '0.2.0',
   DEBUG: true,
   _presentationStructure: null,
   _currentSlide: null,
@@ -34,24 +34,24 @@ window.ns('org.tomasino.clm').modify({
 
   /* Package log method
   */
-  log: function () {
+  log: () => {
     if (org.tomasino.clm.DEBUG) console.log('[org.tomasino.clm]', Array.prototype.join.call(arguments, ' '))
   },
 
   /* Configure startup values
   */
-  initialize: function () {
+  initialize: () => {
     org.tomasino.clm.log('VERSION:', org.tomasino.clm.VERSION)
 
     // Prevent images from dragging. Fixes swipe issues.
-    $(document).bind('dragstart', function () { return false })
-    document.body.addEventListener('touchmove', function (e) { e.preventDefault() }, {passive: false})
+    $(document).bind('dragstart', () => { return false })
+    document.body.addEventListener('touchmove', e => { e.preventDefault() }, {passive: false})
   },
 
   /* Are we in a call?
   */
-  inCall: function () {
-    com.veeva.clm.getDataForCurrentObject('Account', 'ID', function (obj) {
+  inCall: () => {
+    com.veeva.clm.getDataForCurrentObject('Account', 'ID', obj => {
       if (obj.success !== true) {
         org.tomasino.clm.log('Account ID Callback Error', obj)
       } else {
@@ -64,7 +64,7 @@ window.ns('org.tomasino.clm').modify({
 
   /* Per-Account Localstorage store
   */
-  store: function (key, obj) {
+  store: (key, obj) => {
     if (org.tomasino.clm._inCall) {
       var p = org.tomasino.clm._presentationStructure.presentationID
       var a = org.tomasino.clm._accountID
@@ -77,7 +77,7 @@ window.ns('org.tomasino.clm').modify({
 
   /* Per-Account Localstorage get
   */
-  get: function (key) {
+  get: key => {
     if (org.tomasino.clm._inCall) {
       var p = org.tomasino.clm._presentationStructure.presentationID
       var a = org.tomasino.clm._accountID
@@ -98,7 +98,7 @@ window.ns('org.tomasino.clm').modify({
 
   /* Start data processing, routing, etc
   */
-  start: function () {
+  start: () => {
     /* Check for deep links
     */
     var value = window.localStorage.getItem('veevanav')
@@ -121,17 +121,27 @@ window.ns('org.tomasino.clm').modify({
 
   /* Set current slide's ID. Used by navigational methods to know our origin
   */
-  setCurrentSlideID: function (id) {
+  setCurrentSlideID: id => {
     org.tomasino.clm._currentSlide = id
     org.tomasino.clm.log('Current Slide ID:', id)
     org.tomasino.clm.publish(org.tomasino.clm.EVENT_CURRENTSLIDEID, id)
   },
 
-  getCurrentSlideID: function () {
-    return org.tomasino.clm._currentSlide
+  getCurrentSlideID: () => org.tomasino.clm._currentSlide,
+
+  isID: id => {
+    var s = org.tomasino.clm._presentationStructure
+    if (s) {
+      var i = s.slides.length
+      while (i--) {
+        if (s.slides[i].id === id) {
+          return true
+        }
+      }
+    }
   },
 
-  getCurrentSlideKeyMessage: function () {
+  getCurrentSlideKeyMessage: () => {
     var s = org.tomasino.clm._presentationStructure
     if (s) {
       var i = s.slides.length
@@ -145,7 +155,7 @@ window.ns('org.tomasino.clm').modify({
     }
   },
 
-  getCurrentSlideJobCode: function () {
+  getCurrentSlideJobCode: () => {
     var s = org.tomasino.clm._presentationStructure
     if (s) {
       var i = s.slides.length; while (i--) {
@@ -163,7 +173,7 @@ window.ns('org.tomasino.clm').modify({
    * Expects a data object with an array named 'slides' referencing all
    * slides in the current presentation and their Media_File_Name_vod__c
    */
-  navCreate: function (presentationStructure) {
+  navCreate: presentationStructure => {
     if (!('presentationName' in presentationStructure)) {
       org.tomasino.clm.log('Presentation structure missing "presentationName"')
     } else if (!('presentationID' in presentationStructure)) {
@@ -183,7 +193,7 @@ window.ns('org.tomasino.clm').modify({
    * history prior to navigation. Supports old page/state deep linking
    * as version 1. New arbitrary deep linking is version 2.
    */
-  navPrepare: function (deepLink) {
+  navPrepare: deepLink => {
     if (!('version' in deepLink)) {
       org.tomasino.clm.log('DeepLinking version required')
     } else {
@@ -223,7 +233,7 @@ window.ns('org.tomasino.clm').modify({
    *
    * (Optional) pass deep linking object
    */
-  navNext: function (deepLink) {
+  navNext: deepLink => {
     var s = org.tomasino.clm._presentationStructure
     var c = org.tomasino.clm._currentSlide
     if (s) {
@@ -249,7 +259,7 @@ window.ns('org.tomasino.clm').modify({
    *
    * (Optional) pass deep linking object
    */
-  navPrev: function (deepLink) {
+  navPrev: deepLink => {
     var s = org.tomasino.clm._presentationStructure
     var c = org.tomasino.clm._currentSlide
     if (s) {
@@ -275,7 +285,7 @@ window.ns('org.tomasino.clm').modify({
    *
    * (Optional) pass deep linking object
    */
-  navToID: function (id, deepLink) {
+  navToID: (id, deepLink) => {
     var s = org.tomasino.clm._presentationStructure
     if (s) {
       var i = s.slides.length; while (i--) {
@@ -295,7 +305,7 @@ window.ns('org.tomasino.clm').modify({
    *
    * id, type, and description required
    */
-  trackEvent: function (id, type, desc) {
+  trackEvent: (id, type, desc) => {
     var trackingObj = {
       'Track_Element_Id_vod__c': id,
       'Track_Element_Type_vod__c': type,
@@ -313,7 +323,7 @@ window.ns('org.tomasino.clm').modify({
    *
    * id, type, and description required
    */
-  trackUniqueEvent: function (id, type, desc) {
+  trackUniqueEvent: (id, type, desc) => {
     var sig = id + '|||' + type
     if (org.tomasino.clm._oneTimeEvents[sig] === true) return
     org.tomasino.clm._oneTimeEvents[sig] = true
@@ -324,7 +334,7 @@ window.ns('org.tomasino.clm').modify({
    *
    * Returns an observable object with the 'remove" method for easy cleanup
    */
-  subscribe: function (eventName, listener) {
+  subscribe: (eventName, listener) => {
     if (!org.tomasino.clm._events.hasOwnProperty.call(org.tomasino.clm._events, eventName)) {
       org.tomasino.clm._events[eventName] = []
     }
@@ -333,7 +343,7 @@ window.ns('org.tomasino.clm').modify({
 
     // Provide handle back for removal of eventName
     return {
-      remove: function () {
+      remove: () => {
         delete org.tomasino.clm._events[eventName][index]
       }
     }
@@ -343,10 +353,10 @@ window.ns('org.tomasino.clm').modify({
    *
    * (Optional) pass any object along as payload
    */
-  publish: function (eventName, info) {
+  publish: (eventName, info) => {
     if (!org.tomasino.clm._events.hasOwnProperty.call(org.tomasino.clm._events, eventName)) return
 
-    org.tomasino.clm._events[eventName].forEach(function (item) {
+    org.tomasino.clm._events[eventName].forEach(item => {
       item(info !== undefined ? info : {})
     })
   }
